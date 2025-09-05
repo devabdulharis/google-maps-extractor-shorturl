55d1ce027fd073d40b5237808e76903c6dd4dfbb
Abdul Haris <dev.abdulharis@gmail.com>
2025-09-06 04:48:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ function extractPlaceInfoFromHtml(html) {
 
   let match = html.match(/<title>([^<]+)<\/title>/i);
   if (match) {
-    info.name = match[1].replace(/\s*-\s*Google Maps\s*$/i, "");
+    // hapus suffix ' - Google Maps'
+    info.name = match[1].replace(/\s*-\s*Google Maps\s*$/i, "").trim();
   }
 
   // Ambil <meta content="..." property="og:description">
